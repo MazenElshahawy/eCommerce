3ec4d6abed8d631065d022398caf84e991b9611a
MazenElshahawy <Mazen.El-shahawy@outlook.com>
2022-11-17 21:03:15 +0200

    daily edit

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,1 +1,16 @@
-  
+/**********Variables**********/
+const bar = document.querySelector("#bar")
+const navbar = document.querySelector("#navbar")
+const close = document.querySelector("#close")
+
+
+
+/**********Coding**********/
+
+bar.addEventListener("click", (eo) => {
+    navbar.classList.add("active")
+})
+
+close.addEventListener("click", (eo) => {
+    navbar.classList.remove("active")
+})
