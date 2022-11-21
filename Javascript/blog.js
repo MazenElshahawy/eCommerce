1b58188128c2d551c7613516d09431f080523aba
MazenElshahawy <Mazen.El-shahawy@outlook.com>
2022-11-21 15:31:36 +0200

    Daily edit

---
+++ Javascript/blog.js
@@ -0,0 +1,16 @@
+/**********Variables**********/
+const bar = document.querySelector("#bar")
+const navbar = document.querySelector("#navbar")
+const close = document.querySelector("#close")
+
+/**********function of sliding navbar open/close**********/
+
+bar.addEventListener("click", (eo) => {
+    navbar.classList.add("active")
+    eo.preventDefault();
+})
+
+close.addEventListener("click", (eo) => {
+    navbar.classList.remove("active");
+    eo.preventDefault();
+})
